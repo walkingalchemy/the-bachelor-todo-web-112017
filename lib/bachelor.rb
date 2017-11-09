48f542c712878a0dba2bb646add15711bda026e3
James Royer <walkingalchemy@gmail.com>
2017-11-09 23:24:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2101,7 +2101,7 @@ def get_average_age_for_season(data, season)
   total_contestants = 0
   data.each do |seasons, contestants|
     contestants.each do |contestant_hash|
-      age_total += contestant_hash["age"].to_i
+      age_total += contestant_hash["age"].to_f
       total_contestants += 1
     end
   end
